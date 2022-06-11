9e64706d49db57408f89dad55d66504ed47a37db
Dan Corrigan <df.corrigan@gmail.com>
2022-06-10 20:31:04 -0400

    basic react router implementation

--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -10,7 +10,7 @@ const initialValue = [
   },
 ]
 
-const App = () => {
+const Document = () => {
   const editor = React.useMemo(() => withReact(createEditor()), [])
 
   return (
@@ -20,7 +20,37 @@ const App = () => {
   )
 }
 
+const Home = () => {
+  return (
+    <>
+      <h1>Hello. Create a doc.</h1>
+      <Link to="docs">
+        New doc
+      </Link>
+    </>
+  )
+}
+
+// ROUTER code, move later
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="docs" element={<Document />} />
+      <Route path=":documentId" element={<Document />} />
+    </Routes>
+  </BrowserRouter>
+)
+// END ROUTER
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.getElementById('root'))
-  root.render(<App />)
+  root.render(<Router />)
 })
